bf74a2e61a2a474f8558a774e63fe1a1f0ab65ca
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-06-29 15:16:29 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='ictv-plugin-openweek-2018',
+    version='0.1',
+    packages=['ictv.renderer'],
+    package_dir={'ictv': 'ictv'},
+    url='https://github.com/OpenWeek/ictv-plugin-2018',
+    license='TODO',
+    author='TODO',
+    author_email='',
+    description='TODO',
+    include_package_data=True,
+)
