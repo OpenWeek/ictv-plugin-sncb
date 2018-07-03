3dc00a67b84d691ffd22a34a2b913d4e4fb0b811
Guillaume Bellon <bellon@organa.info.ucl.ac.be>
2018-07-03 17:36:23 +0200

    Fonctionnel

--- ictv/plugins/i_like_trains/i_like_trains.py
+++ ictv/plugins/i_like_trains/i_like_trains.py
@@ -24,6 +24,12 @@ from ictv.plugin_manager.plugin_capsule import PluginCapsule
 from ictv.plugin_manager.plugin_manager import get_logger
 from ictv.plugin_manager.plugin_slide import PluginSlide
 from ictv.plugin_manager.plugin_utils import MisconfiguredParameters
+import urllib.request, urllib.error, urllib.parse
+import requests
+import json
+import time
+import datetime
+import web
 
 
 def get_content(channel_id):
@@ -32,16 +38,17 @@ def get_content(channel_id):
     logger = get_logger('i_like_trains', channel)
     departure_station = channel.get_config_param('departure_station')
     duration = channel.get_config_param('duration')
+    language = channel.get_config_param('language')
     if not departure_station:
         logger.warning('Problem with the departure station', extra=logger_extra)  #TODO Verifier gare existante
         return []
     else:
-        return [ILikeTrainsCapsule(departure_station, duration)]
+        return [ILikeTrainsCapsule(departure_station, duration, language)]
 
 
 class ILikeTrainsCapsule(PluginCapsule):
-    def __init__(self, departure_station, duration):
-        self._slides = [ILikeTrainsSlide(departure_station,duration)]
+    def __init__(self, departure_station, duration, language):
+        self._slides = [ILikeTrainsSlide(departure_station, duration, language)]
         self._theme = 'train'
 
     def get_slides(self):
@@ -56,38 +63,56 @@ class ILikeTrainsCapsule(PluginCapsule):
 
 class ILikeTrainsSlide(PluginSlide):
 
-    def __init__(self, departure_station, duration):
+    def __init__(self, departure_station, duration,language):
         self._departure_station = departure_station
         self._duration = duration #TODO Ajouter les heures
-        self._content = {'text-1': {'text': """
+        BASE_URL = "http://api.irail.be/"
+        URLS = {
+            'stations': 'stations',
+            'schedules': 'connections',
+            'liveboard': 'liveboard',
+            'vehicle': 'vehicle'
+        }
 
-        <table style="width:100%">
+        DEFAULT_ARGS = "?format=json"
+        head = {'user-agent': 'ICTVbooyy/0.69 (ictv.github.con; ictv@4.life)'}
+        payload = {'station': departure_station, 'arrdep': 'departures', 'lang': language, 'format': 'json', 'alert': 'true'}
+
+        r = requests.get(BASE_URL + 'liveboard/', params=payload, headers=head)
+        parsed = json.loads(r.text)
+        big_template = """$def with (parsed, actual, departure_station)
+        <table>
             <tr>
-                <th>{}</td>
+                <th>$departure_station</td>
                 <th> </td>
                 <th> </td>
-                <th>{}</td>
+                <th>$actual</td>
             <tr>
                 <th>Destination</th>
                 <th>Voie</th>
                 <th>Départ</th>
                 <th>Retard</th>
             </tr>
+            $for train in parsed['departures']['departure']:
+                <tr>
+                    <td>$train['station']</td>
+                    <td>$train['platform']</td>
+                    <td>$time.strftime('%H:%M', time.localtime(int(train['time'])))</td>
+                    $if int(train['delay']) != 0:
+                        <td>+$(int(train['delay'])//60)'</td>
+                    $else:
+                        <td> </td>
+                </tr>
 
-            <tr>
-                <td>Rome</td>
-                <td>42</td>
-                <td>13:55</td>
-                <td>+55</td>
-            </tr>
-            <tr>
-                <td>Poudlard</td>
-                <td>9.75</td>
-                <td>9:30</td>
-                <td> </td>
-            </tr>
-        </table>
-        """.format(departure_station, '12:00')}, 'title-1': {'text': ''}, 'subtitle-1': {'text': ''}}
+        </table>"""
+        # <td>$time.strftime('%H:%M', time.localtime(int(train['time'])))</td>
+        template_builder = web.template.Template(big_template, globals={'int': int, 'time': time})
+
+        self._content = {'text-1': {'text': str(template_builder(parsed,
+                                                             datetime.datetime.now().strftime('%H:%M'),
+                                                             departure_station))},
+                         'title-1': {'text': ''},
+                         'subtitle-1': {'text': ''}}
 
     def get_duration(self):
         return self._duration
