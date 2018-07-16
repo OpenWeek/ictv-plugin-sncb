ee3388aff2aebccb546700745ff9f1da6f57d44f
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-07-16 22:22:49 +0200

    Fills setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='ictv-plugin-openweek-2018',
+    name='ictv-plugin-sncb',
     version='0.1',
-    packages=['ictv.renderer'],
+    packages=['ictv.plugins.sncb', 'ictv.renderer'],
     package_dir={'ictv': 'ictv'},
-    url='https://github.com/OpenWeek/ictv-plugin-2018',
-    license='TODO',
-    author='TODO',
+    url='https://github.com/OpenWeek/ictv-plugin-sncb',
+    license='AGPL-3.0',
+    author='OpenWeek members',
     author_email='',
-    description='TODO',
+    description='A plugin that brings the real-time information provided by SNCB/NMBS to ICTV',
+    install_requires=['requests'],
     include_package_data=True,
 )
